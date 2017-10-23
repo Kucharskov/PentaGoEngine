b18571b1395deac291c22a28ec940e188b0dfed6
Michał Kucharski <M.Kucharskov@gmail.com>
2017-10-24 00:09:47 +0200

    Opisanie testowego maina

--- Source.cpp
+++ Source.cpp
@@ -2,8 +2,12 @@
 #include "Engine/Game.h"
 
 void main()
+//Testowy main
 {
+	//Instancja silnika gry
 	Game g;
+
+	//Wykonanie ruchow dajacych wygrana dla bialych
 	g.move(0, 1);
 	g.move(1, 1);
 	g.move(0, 2);
@@ -14,6 +18,8 @@ void main()
 	g.move(1, 4);
 	g.move(0, 5);
 	g.move(1, 5);
+
+	//Sprawdzanie wyniku (3 to states::white)
 	std::cout << "Oczekiwany wynik to 3\n";
 	std::cout << "Wynik: " << (int)g.checkWin() << std::endl;
 
