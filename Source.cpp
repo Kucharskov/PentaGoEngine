09a380150cce2391ef96cf3fdbcbe108928366ba
Michał Kucharski <M.Kucharskov@gmail.com>
2017-10-26 16:59:37 +0200

    Rysowanie planszy dla testów

--- Source.cpp
+++ Source.cpp
@@ -6,8 +6,10 @@ int main()
 {
 	//Instancja silnika gry
 	Game g;
-
+	
 	//Wykonanie ruchow dajacych remis
+	g.move(0, 0);
+	g.move(1, 0);
 	g.move(0, 1);
 	g.move(1, 1);
 	g.move(0, 2);
@@ -16,10 +18,30 @@ int main()
 	g.move(1, 3);
 	g.move(0, 4);
 	g.move(1, 4);
-	g.move(0, 5);
-	g.move(1, 5);
 
-	//Sprawdzanie wyniku (3 to states::white)
+	//Rysowanie planszy dla podgladu
+	for (int i = 0; i < g.getSize(); i++) {
+		for (int j = 0; j < g.getSize(); j++) {
+			switch ((int)g.getState(j, i)) {
+			case 0:
+				std::cout << "   ";
+				break;
+			case 1:
+				std::cout << " B ";
+				break;
+			case 2:
+				std::cout << " W ";
+				break;
+			}
+			if (j < g.getSize()-1) std::cout << "|";
+			else std::cout << std::endl;
+		}
+		if (i < g.getSize() - 1) for (int k = 0; k < g.getSize(); k++) std::cout << "--- ";
+		std::cout << std::endl;
+	}
+	
+
+	//Sprawdzanie wyniku (3 to states::draw)
 	std::cout << "Oczekiwany wynik to 3\n";
 	std::cout << "Wynik: " << (int)g.checkWin() << std::endl;
 
